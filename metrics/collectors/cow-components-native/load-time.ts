6ac5010c73ab197ee552598522dd256561f9a00b
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-03-31 14:58:54 +0200

    measure the size of the toggleable bundle

--- metrics/collectors/cow-components-native/load-time.ts
+++ metrics/collectors/cow-components-native/load-time.ts
@@ -3,14 +3,12 @@ import * as path from 'path';
 import { runFunctionIfCalledFromScript } from '../shared/helpers';
 import { storeData } from '../shared/storage';
 import { getLoadTimeForDir } from '../shared/load-time';
-import { FRAMEWORK_NAME, STORE_NAME } from './lib/constants';
-import { DEMO_REPO_DIR } from '../../scripts/lib/cow-components-shared';
+import { STORE_NAME } from './lib/constants';
+import { NATIVE_DEMO_METRICS_TOGGLEABLE_DIR } from '../../scripts/metrics/bundles/cow-components-native';
 
 runFunctionIfCalledFromScript(async () => {
 	await storeData(
 		['metrics', STORE_NAME, 'load-time'],
-		await getLoadTimeForDir(
-			path.join(DEMO_REPO_DIR, FRAMEWORK_NAME, 'metrics', 'empty')
-		)
+		await getLoadTimeForDir(path.join(NATIVE_DEMO_METRICS_TOGGLEABLE_DIR))
 	);
 }, __filename);
--- metrics/collectors/cow-components-svelte/size.ts
+++ metrics/collectors/cow-components-svelte/size.ts
@@ -1,21 +1,15 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import { DEMO_REPO_DIR } from '../../scripts/lib/cow-components-shared';
 
+import { SVELTE_DEMO_METRICS_TOGGLEABLE_DIR } from '../../scripts/metrics/bundles/cow-components-svelte';
 import { runFunctionIfCalledFromScript } from '../shared/helpers';
 import { storeData } from '../shared/storage';
-import { FRAMEWORK_NAME, STORE_NAME } from './lib/constants';
+import { STORE_NAME } from './lib/constants';
 
 export async function getDashboardSize(): Promise<number> {
 	return (
 		await fs.stat(
-			path.join(
-				DEMO_REPO_DIR,
-				FRAMEWORK_NAME,
-				'metrics',
-				'empty',
-				'index.bundle.js'
-			)
+			path.join(SVELTE_DEMO_METRICS_TOGGLEABLE_DIR, 'index.bundle.js')
 		)
 	).size;
 }
