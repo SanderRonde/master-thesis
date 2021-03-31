2c5eff207fc49b9c43a2f6db8abb54a92c0118ce
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-03-31 17:03:47 +0200

    remove unneeded code

--- metrics/collectors/cow-components-svelte/templates/render-time-index-html-template.ts
+++ metrics/collectors/cow-components-svelte/templates/render-time-index-html-template.ts
@@ -1,6 +1,3 @@
-import { getJoinedComponentDefs } from '../../dashboard/lib/render-time/generate-render-time-page';
-
 export async function getRenderTimeIndexHTMLTemplate() {
-	const components = await getJoinedComponentDefs();
 	return `<html><head><link rel="stylesheet" href="./index.css" /></head><body style="margin: 50px;"><div style="margin-top: 300px;" id="root"></div><script src="index.bundle.js"></script></body></html>`;
 }
