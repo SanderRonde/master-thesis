a15d7ff35ca6b7d938cd2e06c8e20501b21f99a9
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-04-01 14:54:00 +0200

    add option to log debug logs in prod mode

--- metrics/collectors/shared/log.ts
+++ metrics/collectors/shared/log.ts
@@ -100,7 +100,7 @@ export function error(filePath: string, ...data: [any, ...any]) {
 }
 
 export function debug(filePath: string, ...data: [any, ...any]) {
-	if (!DEVELOPMENT) {
+	if (!DEVELOPMENT && !process.argv.includes('--log-debug')) {
 		return;
 	}
 	const raw = `[ ${getTagFromFilePath(filePath)} ] -`;
