b1b44ec52d6d99464252257fba943b5db449c171
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-04-15 13:53:54 +0200

    remove export

--- metrics/collectors/cow-components/dashboard/number-of-components.ts
+++ metrics/collectors/cow-components/dashboard/number-of-components.ts
@@ -2,7 +2,7 @@ import { runFunctionIfCalledFromScript } from '../../shared/helpers';
 import { storeData } from '../../shared/storage';
 import { getComponents } from './lib/get-components';
 
-export async function getNumberOfComponents(): Promise<number> {
+async function getNumberOfComponents(): Promise<number> {
 	return (await getComponents()).length;
 }
 
