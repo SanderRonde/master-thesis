b146d9fda9dd59f529d6d12af35981138ff7cfbf
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-04-15 13:55:43 +0200

    remove export

--- metrics/collectors/cow-components/dashboard/size.ts
+++ metrics/collectors/cow-components/dashboard/size.ts
@@ -5,7 +5,7 @@ import { runFunctionIfCalledFromScript } from '../../shared/helpers';
 import { storeData } from '../../shared/storage';
 import { DASHBOARD_DIST_DIR } from './lib/constants';
 
-export async function getDashboardSize(): Promise<number> {
+async function getDashboardSize(): Promise<number> {
 	return (await fs.stat(path.join(DASHBOARD_DIST_DIR, 'bundle.js'))).size;
 }
 
