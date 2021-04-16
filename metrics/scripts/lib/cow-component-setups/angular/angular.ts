4597d2f37bc21fd6d1bbf9a65d8a61cde9a2d556
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-04-16 19:15:52 +0200

    always re-generate toggleable bundle

--- metrics/scripts/lib/cow-component-setups/angular/angular.ts
+++ metrics/scripts/lib/cow-component-setups/angular/angular.ts
@@ -38,7 +38,7 @@ export function createAngularSetupCommand(
 		metricsComponentDir,
 	} = getAngularDirs(baseDir);
 
-	return registerSetupCommand(commandName).run(async (exec, args) => {
+	return registerSetupCommand(commandName).run(async (exec) => {
 		/**
 		 * The angular compiler requires a TS version
 		 * >= 3.9.2 and < 4.1, so we use 4.0.7
@@ -47,63 +47,55 @@ export function createAngularSetupCommand(
 		const demoDirCtx = await exec(`cd ${frameworkDemoDir}`);
 		await demoDirCtx.keepContext('npm install typescript@4.0.7');
 
-		if (!(await fs.pathExists(angularDemoDist)) || args['no-cache']) {
-			await rimrafAsync(demoMetricsDir);
-			await exec('? Generating toggleable bundle');
+		await rimrafAsync(demoMetricsDir);
+		await exec('? Generating toggleable bundle');
 
-			await exec('? Generating JS');
-			const jsFilePath = path.join(
-				metricsComponentDir,
-				'metrics.component.ts'
-			);
-			const jsContent = await getRenderTimeJsTemplate(submoduleName);
-			await writeFile(jsFilePath, jsContent);
+		await exec('? Generating JS');
+		const jsFilePath = path.join(
+			metricsComponentDir,
+			'metrics.component.ts'
+		);
+		const jsContent = await getRenderTimeJsTemplate(submoduleName);
+		await writeFile(jsFilePath, jsContent);
 
-			await exec('? Generating HTML');
-			const htmlFilePath = path.join(
-				metricsComponentDir,
-				'metrics.component.html'
-			);
-			const htmlContent = await getRenderTimeHTMLTemplate(submoduleName);
-			await writeFile(htmlFilePath, htmlContent);
+		await exec('? Generating HTML');
+		const htmlFilePath = path.join(
+			metricsComponentDir,
+			'metrics.component.html'
+		);
+		const htmlContent = await getRenderTimeHTMLTemplate(submoduleName);
+		await writeFile(htmlFilePath, htmlContent);
 
-			await exec('? Copying CSS');
-			await fs.copy(
-				path.join(
-					frameworkDemoDir,
-					'packages/angular/assets/cow-components.css'
-				),
-				path.join(frameworkDemoDir, 'src/assets', 'cow-components.css')
-			);
+		await exec('? Copying CSS');
+		await fs.copy(
+			path.join(
+				frameworkDemoDir,
+				'packages/angular/assets/cow-components.css'
+			),
+			path.join(frameworkDemoDir, 'src/assets', 'cow-components.css')
+		);
 
-			await exec('? Changing demo-project to use new component');
-			const appModulePath = path.join(
-				metricsComponentDir,
-				'app.module.ts'
-			);
-			const appModuleContent = await readFile(appModulePath);
-			await writeFile(
-				appModulePath,
-				appModuleContent.replace(
-					'./app.component',
-					'./metrics.component'
-				)
-			);
-			const mainFilePath = path.join(frameworkDemoDir, 'src/main.ts');
-			const mainFileContent = await readFile(mainFilePath);
-			await writeFile(
-				mainFilePath,
-				mainFileContent.replace('/app.component', '/metrics.component')
-			);
+		await exec('? Changing demo-project to use new component');
+		const appModulePath = path.join(metricsComponentDir, 'app.module.ts');
+		const appModuleContent = await readFile(appModulePath);
+		await writeFile(
+			appModulePath,
+			appModuleContent.replace('./app.component', './metrics.component')
+		);
+		const mainFilePath = path.join(frameworkDemoDir, 'src/main.ts');
+		const mainFileContent = await readFile(mainFilePath);
+		await writeFile(
+			mainFilePath,
+			mainFileContent.replace('/app.component', '/metrics.component')
+		);
 
-			await exec('? Bundling');
-			const demoCtx = await exec(`cd ${frameworkDemoDir}`);
-			await demoCtx.keepContext('ng build angular-demo');
+		await exec('? Bundling');
+		const demoCtx = await exec(`cd ${frameworkDemoDir}`);
+		await demoCtx.keepContext('ng build angular-demo');
 
-			await exec('? Changing back used component for demo-project');
-			await writeFile(appModulePath, appModuleContent);
-			await writeFile(mainFilePath, mainFileContent);
-		}
+		await exec('? Changing back used component for demo-project');
+		await writeFile(appModulePath, appModuleContent);
+		await writeFile(mainFilePath, mainFileContent);
 
 		await exec('? Bundling up built files for measuring');
 		await rimrafAsync(angularMetadataBundle);
